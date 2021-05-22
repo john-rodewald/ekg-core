851cb518daf26a69a6824ed2d1bbfcd8d5aeffdf
Alex Chen <awchen@fastmail.com>
2021-05-21 22:41:11 -0600

    Use "criterion" for benchmarks

--- benchmarks/Counter.hs
+++ benchmarks/Counter.hs
@@ -3,16 +3,31 @@
 -- | Perform 100,000 atomic increments using 100 concurrent writers.
 module Main where
 
+import Prelude hiding (read)
+
 import Control.Concurrent
 import Control.Monad
+import Criterion
+import Criterion.Main (defaultMain)
+import Data.Int (Int64)
 import System.Metrics.Counter
 
 main :: IO ()
-main = do
+main = defaultMain
+  [ bench "Increment counter with multiple writers" $
+      whnfIO incrementWithMultipleWriters
+  ]
+
+incrementWithMultipleWriters :: IO Int64
+incrementWithMultipleWriters = do
     counter <- new
     locks <- replicateM n newEmptyMVar
     mapM_ (forkIO . work counter iters) locks
     mapM_ takeMVar locks
+    total <- read counter
+    unless (fromIntegral total == n*iters) $
+      error "Incorrect count!"
+    pure total
   where
     n = 100
     iters = 100000
--- benchmarks/Distribution.hs
+++ benchmarks/Distribution.hs
@@ -4,16 +4,31 @@
 -- writers.
 module Main where
 
+import Prelude hiding (read)
+
 import Control.Concurrent
 import Control.Monad
+import Criterion
+import Criterion.Main (defaultMain)
+import Data.Int (Int64)
 import System.Metrics.Distribution
 
 main :: IO ()
-main = do
+main = defaultMain
+  [ bench "Add to distribution with multiple writers" $
+      whnfIO addWithMultipleWriters
+  ]
+
+addWithMultipleWriters :: IO Int64
+addWithMultipleWriters = do
     distrib <- new
     locks <- replicateM n newEmptyMVar
     mapM_ (forkIO . work distrib iters) locks
     mapM_ takeMVar locks
+    total <- count <$> read distrib
+    unless (fromIntegral total == n*iters) $
+      error "Incorrect count!"
+    pure total
   where
     n = 100
     iters = 100000
